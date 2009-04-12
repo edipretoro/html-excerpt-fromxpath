e7e54b9ca3f621d15ea3527e53952966169ff34f
manu <manu@edgeoya.(none)>
2009-04-12 23:11:08 +0200

    Add a sample scraper using this module

---
+++ eg/scrape_wikipedia.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/env perl -w
+
+use strict;
+use warnings;
+
+use lib './../lib';
+use HTML::Excerpt::FromXPath;
+
+my $scraper = HTML::Excerpt::FromXPath->new();
+
+my $uri = shift or die "Usage: $0 http://*.wikipedia.org/wiki/Page\n";
+
+die "$0 only handle URI from Wikipedia websites\n"
+  unless $uri =~ /^http:\/\/*.wikipedia.org\/wiki\//;
+
+my $page = $parser->scrape(
+    url   => $uri,
+    xpath => '//'
+);
+
