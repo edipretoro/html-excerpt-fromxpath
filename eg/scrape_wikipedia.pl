b93181148729721a1e612a92129ff2fa51196346
Emmanuel Di Pretoro <edipretoro@gmail.com>
2009-04-13 11:31:38 +0200

    - correct the regexp to check if it's a URL from Wikipedia - update the use of the returned value of 'scrape'

--- eg/scrape_wikipedia.pl
+++ eg/scrape_wikipedia.pl
@@ -11,12 +11,12 @@ my $scraper = HTML::Excerpt::FromXPath->new();
 my $uri = shift or die "Usage: $0 http://*.wikipedia.org/wiki/Page\n";
 
 die "$0 only handle URI from Wikipedia websites\n"
-  unless $uri =~ /^http:\/\/*.wikipedia.org\/wiki\//;
+  unless $uri =~ /.*.wikipedia.org\/wiki\//;
 
 my $page = $scraper->scrape(
     url   => $uri,
     xpath => '//div[@id="content"]'
 );
 
-print $page->as_HTML . "\n";
+print $page->[0]->as_HTML . "\n";
 
