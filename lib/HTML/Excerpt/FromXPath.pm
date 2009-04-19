ed0357f98214f7ad7d737700649cf26b77b5e56a
Emmanuel Di Pretoro <edipretoro@gmail.com>
2009-04-19 17:22:45 +0200

    Change $VERSION to 0.02

--- lib/HTML/Excerpt/FromXPath.pm
+++ lib/HTML/Excerpt/FromXPath.pm
@@ -19,11 +19,11 @@ HTML::Excerpt::FromXPath - Select a part of a HTML file with an XPath expression
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 =head1 SYNOPSIS
